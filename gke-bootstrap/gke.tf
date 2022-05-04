36f1acfe28c7ee0c8c044c0a0f0de3c7db337a7c
Tanmay Bhat <tanmay.bhat@unbxd.com>
2022-05-04 11:31:23 +0530

    add base files

---
+++ gke-bootstrap/gke.tf
@@ -0,0 +1,45 @@
+#GKE cluster
+module "gke" {
+  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  project_id             = var.project_id
+  name                   = "${var.cluster_name}-${var.env_name}"
+  regional               = false
+  region                 = var.region
+  zones                  = ["us-central1-a"]
+  network                = module.vpc.network_name
+  subnetwork             = module.vpc.subnets_names[0]
+  ip_range_pods          = "${var.cluster_name}-${var.env_name}-ip-range-pods"	
+  ip_range_services      = "${var.cluster_name}-${var.env_name}-ip-range-services"	
+  enable_private_endpoint    = false
+  enable_private_nodes       = true 
+  master_ipv4_cidr_block     = "10.0.0.0/28" 
+  depends_on             = [module.vpc.network_name]
+  remove_default_node_pool =  true
+  logging_service        = "none"
+  monitoring_service     = "none"
+  node_pools = [
+    {
+      name                      = "${var.cluster_name}-${var.env_name}-primary-nodepool"
+      machine_type              = "e2-medium"
+      node_locations            = "us-central1-a"
+      min_count                 = var.minnode
+      max_count                 = var.maxnode
+      disk_size_gb              = var.disksize
+      preemptible               = false
+    },
+  ]
+}
+
+#GKE Auth
+module "gke_auth" {
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  project_id   = var.project_id
+  location     = module.gke.location
+  cluster_name = module.gke.name
+  depends_on   = [module.gke.name]
+}
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig-${var.cluster_name}-${var.env_name}"
+  depends_on = [module.gke_auth]
+}
---
+++ gke-bootstrap/variables.tf
@@ -0,0 +1,67 @@
+#==========================GKE=========================
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+}
+variable "cluster_name" {
+  description = "The name for the GKE cluster to create"
+  default = "fluxcd-demo"
+}
+variable "env_name" {
+  description = "The environment for the GKE cluster : Dev/Staging/Prod"
+  default     = "dev"
+}
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "us-central1"
+}
+
+variable "minnode" {
+  description = "Mininum number of nodes in node-pool"
+  default     = "2"
+}
+
+variable "maxnode" {
+  description = "Maximum number of nodes in node-pool"
+  default     = "3"
+}
+
+variable "disksize" {
+  description = "Disk Size in GB"
+  default     = "25"
+}
+
+#==========================Flux & Github==================
+
+variable "github_owner" {
+  type        = string
+  description = "Github username in which the repository exists"
+}
+
+variable "github_token" {
+  type        = string
+  description = "github token"
+}
+
+variable "repository_name" {
+  type        = string
+  default     = "fluxcd-demo"
+  description = "The Github repository name in which FluxCD configs exists"
+}
+
+variable "repository_visibility" {
+  type        = string
+  default     = "public"
+  description = "How visible is the github repo, Public/Private"
+}
+
+variable "branch" {
+  type        = string
+  default     = "main"
+  description = "The Branch name in which FluxCD configs exists"
+}
+
+variable "target_path" {
+  type        = string
+  default     = "./clusters/staging"
+  description = "flux sync target path"
+}
